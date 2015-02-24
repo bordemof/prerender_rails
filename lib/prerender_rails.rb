c5a9b9c9550b58927114c1f60b5ea02b9b3edd82
Imanol Yáñez Sastre <lonami80@gmail.com>
2015-02-24 13:40:09 +0100

    Utm detection

--- lib/prerender_rails.rb
+++ lib/prerender_rails.rb
@@ -140,8 +140,8 @@ module Rack
         structure.shift
         #UTM PATCH
         if structure[0].include? "scaped_fragment"
-          env['UTM'] = structure.shift
-          puts "UTM DETECTED :",env['UTM']
+          utm = structure.shift
+          puts "UTM DETECTED :", utm
         end
         puts "PRERENDER TRANSFORMING TO ...",'/seo/'+structure.join('/')
         env['PATH_INFO'] = '/seo/'+structure.join('/')
